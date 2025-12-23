8e3e0fffd4c3393e32e354fa04637479cadc5f59
s114300-ariel <s114300@ms2.kghs.kh.edu.tw>
2025-12-23 11:47:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let funnyTexts = [
+  "你偷看答案對不對 😏",
+  "這不是運氣，是實力 💪",
+  "數學之神降臨 😂",
+  "太強了吧 😎",
+  "要不要去買樂透阿 🎰"
+];
+
 let answer = Math.floor(Math.random() * 100) + 1;
 let attempts = 0;
 
@@ -9,13 +17,26 @@ function checkNumber() {
   let guess = Number(input.value);
   attempts++;
 
-  if (guess > answer) {
-    result.textContent = "太大了！";
-  } else if (guess < answer) {
-    result.textContent = "太小了！";
-  } else {
-    result.textContent = "🎉 答對了！";
-  }
+ if (guess > answer) {
+  result.textContent = "太大了！";
+  result.style.color = "red";
+  document.getElementById("funnyImg").style.display = "none";
+}
+else if (guess < answer) {
+  result.textContent = "太小了！";
+  result.style.color = "red";
+  document.getElementById("funnyImg").style.display = "none";
+}
+else {
+  let randomText = funnyTexts[Math.floor(Math.random() * funnyTexts.length)];
+
+  result.innerHTML = "🎉🎉 猜對了！！<br>" + randomText;
+  result.style.color = "green";
+
+  document.getElementById("funnyImg").style.display = "block";
+}
+
+
 
   count.textContent = "猜測次數：" + attempts;
 }
